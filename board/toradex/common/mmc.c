11f1c6ec18e2c66c29105feca4ca5a632db1d870
Max Krummenacher <max.krummenacher@toradex.com>
2020-02-21 14:56:39 +0100

    board: toradex: Fix fastboot build
    NXP common code now calls a board function mmc_map_to_kernel_blk().
    Provide a weak function in common code for boards not requiring
    special handling.
    
    Introduced by:
    commit 75f2d65ebf90 ("MLK-18591-3 android: Add FSL android fastboot support")
    commit 3000cd67e063 ("MA-20238 Pass boot device from bootloader")
    
    Prevents:
    aarch64-none-linux-gnu-ld.bfd: boot/image-android.c:428: undefined reference to `mmc_map_to_kernel_blk'
    
    Taken from boards/freescale/common/mmc.c
    
    NXP downstream specific
    
    Upstream-Status: Inappropriate [other]
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

---
+++ board/toradex/common/mmc.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2016-2019 Toradex, Inc.
+ */
+
+#include <mmc.h>
+
+/* provide a default implementation */
+__weak int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
+}
