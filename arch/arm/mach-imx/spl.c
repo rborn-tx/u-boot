rborn-tx/u-boot

commit 9de35448580020b526b6e5a8b538a59a64c72f0e

imx: spl: implement spl_load_simple_fit_fix_load
read the address where the IVT header must sit
from IVT image header, loaded from SPL into
an malloced buffer and copy the IVT header
to this address

May make this dependend on SoC ?

Signed-off-by: Heiko Schocher <hs@denx.de>

arch/arm/mach-imx/spl.c
@@ -345,3 +345,36 @@ int dram_init_banksize(void)
 	return 0;
 }
 #endif
+
+/*
+ * read the address where the IVT header must sit
+ * from IVT image header, loaded from SPL into
+ * an malloced buffer and copy the IVT header
+ * to this address
+ */
+void *spl_load_simple_fit_fix_load(const void *fit)
+{
+	struct ivt *ivt;
+	unsigned long new;
+	unsigned long offset;
+	unsigned long size;
+	u8 *tmp = (u8 *)fit;
+
+	offset = ALIGN(fdt_totalsize(fit), 0x1000);
+	size = ALIGN(fdt_totalsize(fit), 4);
+	size = board_spl_fit_size_align(size);
+	tmp += offset;
+	ivt = (struct ivt *)tmp;
+	if (ivt->hdr.magic != IVT_HEADER_MAGIC) {
+		debug("no IVT header found\n");
+		return (void *)fit;
+	}
+	debug("%s: ivt: %p offset: %lx size: %lx\n", __func__, ivt, offset, size);
+	debug("%s: ivt self: %x\n", __func__, ivt->self);
+	new = ivt->self;
+	new -= offset;
+	debug("%s: new %lx\n", __func__, new);
+	memcpy((void *)new, fit, size);
+
+	return (void *)new;
+}

commit 3428b470191ca0f5bb48d95e77e8ba48d9708a42

imx: spl: fix u-boot fit loading
Override weak spl_load_simple_fix_load function only when necessary
to manage signature verification via High Assurance Boot (HAB).

In particular if spl_load_simple_fix_load function is overridden also
the board_spl_fit_size_align must be overridden to provide the correct
buffer size.

This ensures correct sizing of the upload buffer and prevents buffer
overflows which may lead to memory corruption and system reset.

Since an IVT header is checked using just a byte magic number, and the
memory beyond the buffer size is random, this fix addresses an issue
where the presence of an apparently valid IVT header beyond the buffer
size could result in unintended memcpy operations to random memory
addresses, often leading to system reset.

Upstream-Status: Inappropriate [downstream specific]

Upstream U-Boot fixed this differently in combination with binman to
create the final bootcontainer.
Commit 6039e0edc854 ("imx: hab: Simplify the mechanism")

Fixes: 9de354485800 ("imx: spl: implement spl_load_simple_fit_fix_load")
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

arch/arm/mach-imx/spl.c
@@ -352,7 +352,8 @@ int dram_init_banksize(void)
 }
 #endif
 
-#if IS_ENABLED(CONFIG_SPL_LOAD_FIT)
+#if IS_ENABLED(CONFIG_SPL_LOAD_FIT) && IS_ENABLED(CONFIG_IMX_HAB) && \
+	!IS_ENABLED(CONFIG_SPL_FIT_SIGNATURE)
 static int spl_verify_fit_hash(const void *fit)
 {
 	unsigned long size;