jake-freeman/ti-u-boot

commit 0af785dd3f83d91c478a177dc6897519787ade41

arm: mach-k3: common: Add lpm_resume_from_ddr
This is a common function that helps to resume from DDR. There are two
pointers which are fetched from DDR to resume, the TIFS context pointer
which points to the context in DDR. There is another pointer to the DM
loadaddr to jump back into DM.

The DM loadaddr is stored during a normal boot to be reused during
wakeup.

Signed-off-by: Markus Schneider-Pargmann <msp@baylibre.com>

arch/arm/mach-k3/common.c
@@ -178,6 +178,47 @@ bool wkup_ctrl_is_lpm_exit(void)
 		wkup_ctrl_canuart_magic_word_set();
 }
 
+#if IS_ENABLED(CONFIG_K3_IODDR)
+static int lpm_restore_context(u64 ctx_addr)
+{
+	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
+	int ret;
+
+	ret = ti_sci->ops.lpm_ops.restore_context(ti_sci, ctx_addr);
+	if (ret)
+		printf("Failed to restore context from DDR %d\n", ret);
+
+	return ret;
+}
+
+struct lpm_meta_data {
+	u64 dm_jump_address;
+	u64 tifs_context_save_address;
+	u64 reserved[30];
+} __packed__;
+
+void __noreturn lpm_resume_from_ddr(void)
+{
+	typedef void __noreturn (*image_entry_noargs_t)(void);
+	image_entry_noargs_t image_entry;
+	int ret;
+	struct lpm_meta_data *lpm_data = (struct lpm_meta_data *)CONFIG_K3_R5F_LPM_META_DATA;
+
+	ret = lpm_restore_context(lpm_data->tifs_context_save_address);
+	if (ret)
+		panic("Failed to restore context from 0x%p\n",
+		      (void *)lpm_data->tifs_context_save_address);
+
+	image_entry = (image_entry_noargs_t)(u64 *)lpm_data->dm_jump_address;
+	printf("Resuming from DDR, jumping to stored DM loadaddr 0x%p\n",
+	       image_entry);
+
+	image_entry();
+}
+#else
+void lpm_resume_from_ddr(void) {}
+#endif
+
 bool is_rom_loaded_sysfw(struct rom_extended_boot_data *data)
 {
 	if (strncmp(data->header, K3_ROM_BOOT_HEADER_MAGIC, 7))

arch/arm/mach-k3/common.h
@@ -47,6 +47,7 @@ void do_board_detect(void);
 void ti_secure_image_check_binary(void **p_image, size_t *p_size);
 void wkup_ctrl_remove_can_io_isolation_if_set(void);
 bool wkup_ctrl_is_lpm_exit(void);
+void lpm_resume_from_ddr(void);
 
 #if (IS_ENABLED(CONFIG_K3_QOS))
 void setup_qos(void);

rborn-tx/u-boot

commit 2e5c602ce32a3a2b8d38320dbf2996150992e457

arm: mach-k3: common: fix build warnings
Enabling CONFIG_K3_IODDR results in the following warnings during the U-Boot
R5 compilation from the lpm_resume_from_ddr function. These warnings cause
strict CI/CD pipeline failures, as warnings are treated as errors.
Resolve these warnings without affecting the functionality.

arch/arm/mach-k3/common.c: In function ‘lpm_resume_from_ddr’:
arch/arm/mach-k3/common.c:242:23: warning: cast to pointer from integer of
different size [-Wint-to-pointer-cast]
            (void *)lpm_data->tifs_context_save_address);
            ^
arch/arm/mach-k3/common.c:244:45: warning: cast to pointer from integer of
different size [-Wint-to-pointer-cast]
            image_entry = (image_entry_noargs_t)(u64 *)lpm_data->dm_jump_address;
                                                ^
arch/arm/mach-k3/common.c:246:29: warning: cast to pointer from integer of
different size [-Wint-to-pointer-cast]
            image_entry, (void *)lpm_data->tifs_context_save_address);
            ^

Upstream-Status: Inappropriate [other]
Fixes: 0af785dd3f83 ("arm: mach-k3: common: Add lpm_resume_from_ddr")
Signed-off-by: Parth Pancholi <parth.pancholi@toradex.com>

arch/arm/mach-k3/common.c
@@ -238,12 +238,12 @@ void __noreturn lpm_resume_from_ddr(void)
 
 	ret = lpm_restore_context(lpm_data->tifs_context_save_address);
 	if (ret)
-		panic("Failed to restore context from 0x%p\n",
-		      (void *)lpm_data->tifs_context_save_address);
+		panic("Failed to restore context from 0x%llx\n",
+		      (u64)lpm_data->tifs_context_save_address);
 
-	image_entry = (image_entry_noargs_t)(u64 *)lpm_data->dm_jump_address;
-	printf("Resuming from DDR, jumping to stored DM loadaddr 0x%p, TIFS context restored from 0x%p\n",
-	       image_entry, (void *)lpm_data->tifs_context_save_address);
+	image_entry = (image_entry_noargs_t)(uintptr_t)lpm_data->dm_jump_address;
+	printf("Resuming from DDR, jumping to stored DM loadaddr 0x%p, TIFS context restored from 0x%llx\n",
+	       image_entry, (u64)lpm_data->tifs_context_save_address);
 
 	image_entry();
 }