rborn-tx/u-boot

commit 8b13c1698fd66646ab6a5962a6227fb2918db9b6

board: toradex: Add initial support for toradex smarc imx95
Add initial U-Boot support for Toradex SMARC iMX95 SoM.

Upstream-Status: Pending
Signed-off-by: João Paulo Gonçalves <joao.goncalves@toradex.com>

board/toradex/smarc-imx95/spl.c
@@ -1,0 +1,89 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2025 Toradex */
+
+#include <asm/arch/ccm_regs.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch-imx9/mu.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/ele_api.h>
+#include <asm/sections.h>
+#include <asm/global_data.h>
+#include <clk.h>
+#include <dt-bindings/clock/fsl,imx95-clock.h>
+#include <dt-bindings/power/fsl,imx95-power.h>
+#include <hang.h>
+#include <init.h>
+#include <log.h>
+#include <scmi_agent.h>
+#include <scmi_protocols.h>
+#include <spl.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int spl_board_boot_device(enum boot_device boot_dev_spl)
+{
+	switch (boot_dev_spl) {
+	case SD1_BOOT:
+	case MMC1_BOOT:
+		return BOOT_DEVICE_MMC1;
+	case SD2_BOOT:
+	case MMC2_BOOT:
+		return BOOT_DEVICE_MMC2;
+	case USB_BOOT:
+		return BOOT_DEVICE_BOARD;
+	case QSPI_BOOT:
+		return BOOT_DEVICE_SPI;
+	default:
+		return BOOT_DEVICE_NONE;
+	}
+}
+
+void spl_board_init(void)
+{
+	int ret;
+
+	ret = ele_start_rng();
+	if (ret)
+		printf("Fail to start RNG: %d\n", ret);
+}
+
+void board_init_f(ulong dummy)
+{
+	u32 state = 0;
+	int ret;
+
+	if (IS_ENABLED(CONFIG_SPL_RECOVER_DATA_SECTION) && IS_ENABLED(CONFIG_SPL_BUILD))
+		spl_save_restore_data();
+
+	timer_init();
+
+	/* Need dm_init() to run before any SCMI calls */
+	spl_early_init();
+
+	/* Need to enable SCMI drivers and ELE driver before console */
+	ret = imx9_probe_mu();
+	if (ret)
+		hang(); /* MU not probed, nothing can be outputed, hang */
+
+	arch_cpu_init();
+
+	board_early_init_f();
+
+	preloader_console_init();
+
+	printf("SOC: 0x%x\n", gd->arch.soc_rev);
+	printf("LC: 0x%x\n", gd->arch.lifecycle);
+
+	/* Set ARM CPU freq to max rate */
+	clock_init_late();
+
+	/* Check DDR MIX is already powered up */
+	ret = scmi_pwd_state_get(gd->arch.scmi_dev, IMX95_PD_DDR, &state);
+	if (ret)
+		printf("scmi_pwd_state_get Failed %d for DDRMIX\n", ret);
+	else if (state == BIT(30))
+		panic("DDRMIX is powered OFF, Please initialize DDR with OEI\n");
+
+	board_init_r(NULL, 0);
+}

arch/arm/mach-imx/imx9/Kconfig
@@ -82,6 +82,10 @@ config TARGET_IMX95_15X15_EVK
 	bool "imx95_15x15_evk"
 	select IMX95
 
+config TARGET_TORADEX_SMARC_IMX95
+	bool "Support Toradex SMARC iMX95"
+	select IMX95
+
 endchoice
 
 source "board/freescale/imx91_evk/Kconfig"
@@ -91,6 +95,7 @@ source "board/phytec/phycore_imx93/Kconfig"
 source "board/variscite/imx93_var_som/Kconfig"
 source "board/freescale/imx93_qsb/Kconfig"
 source "board/freescale/imx95_evk/Kconfig"
+source "board/toradex/smarc-imx95/Kconfig"
 source "board/toradex/verdin-imx95/Kconfig"
 
 endif

board/toradex/common/tdx-cfg-block.c
@@ -164,6 +164,7 @@ const struct toradex_som toradex_modules[] = {
 	{ APALIS_IMX8QP_WIFI_BT_1300MHZ,         "Apalis iMX8QP 2GB WB",                 TARGET_IS_ENABLED(APALIS_IMX8)     },
 	{ APALIS_IMX8QP_1300MHZ,                 "Apalis iMX8QP 2GB",                    TARGET_IS_ENABLED(APALIS_IMX8)     },
 	{ APALIS_IMX8QM_8GB_WIFI_BT_IT_1300MHZ,  "Apalis iMX8QM 8GB WB IT",              TARGET_IS_ENABLED(APALIS_IMX8)     },
+	{ SMARC_IMX95_HEXA_8GB_WB_IT,            "SMARC iMX95 Hexa 8GB WB IT",           TARGET_IS_ENABLED(TORADEX_SMARC_IMX95) },
 };
 
 struct pid4list {

board/toradex/common/tdx-cfg-block.h
@@ -122,6 +122,7 @@ enum {
 	APALIS_IMX8QP_WIFI_BT_1300MHZ,
 	APALIS_IMX8QP_1300MHZ,
 	APALIS_IMX8QM_8GB_WIFI_BT_IT_1300MHZ, /* 95 */
+	SMARC_IMX95_HEXA_8GB_WB_IT,
 };
 
 enum {

board/toradex/smarc-imx95/Kconfig
@@ -1,0 +1,36 @@
+if TARGET_TORADEX_SMARC_IMX95
+
+config SYS_BOARD
+	default "smarc-imx95"
+
+config SYS_VENDOR
+	default "toradex"
+
+config SYS_CONFIG_NAME
+	default "toradex-smarc-imx95"
+
+config TDX_CFG_BLOCK
+	default y
+
+config TDX_CFG_BLOCK_2ND_ETHADDR
+	default y
+
+config TDX_CFG_BLOCK_DEV
+	default "0"
+
+# Toradex config block in eMMC, at the end of 1st "boot sector"
+config TDX_CFG_BLOCK_OFFSET
+	default "-512"
+
+config TDX_CFG_BLOCK_PART
+	default "1"
+
+config TDX_HAVE_EEPROM_EXTRA
+	default y
+
+config TDX_HAVE_MMC
+	default y
+
+source "board/toradex/common/Kconfig"
+
+endif

board/toradex/smarc-imx95/MAINTAINERS
@@ -1,0 +1,11 @@
+Toradex SMARC iMX95
+F:	arch/arm/dts/imx95-toradex-smarc.dtsi
+F:	arch/arm/dts/imx95-toradex-smarc-dev.dts
+F:	arch/arm/dts/imx95-toradex-smarc-dev-u-boot.dtsi
+F:	board/toradex/smarc-imx95/
+F:	configs/toradex-smarc-imx95_defconfig
+F:	doc/board/toradex/smarc-imx95.rst
+F:	include/configs/toradex-smarc-imx95.h
+M:	Francesco Dolcini <francesco.dolcini@toradex.com>
+S:	Maintained
+W:	https://www.toradex.com/computer-on-modules/smarc-arm-family/nxp-imx95

board/toradex/smarc-imx95/Makefile
@@ -1,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2025 Toradex
+
+obj-y += smarc-imx95.o
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+endif
+
+# TODO: For the SMARC Dev Board, the USB TCPC code must be removed, the below
+# file was included only to test on the Verdin-EVK ans has the TCPC functions
+# declarations.
+obj-y += ../../freescale/common/

board/toradex/smarc-imx95/smarc-imx95.c
@@ -1,0 +1,288 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (C) 2025 Toradex */
+
+#include <asm/arch-imx9/ccm_regs.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <dt-bindings/power/fsl,imx95-power.h>
+#include <dwc3-uboot.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <init.h>
+#include <linux/bitfield.h>
+#include <linux/delay.h>
+#include <linux/bitops.h>
+#include <scmi_protocols.h>
+#include <usb.h>
+
+#include "../common/tdx-cfg-block.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/* TODO: For the SMARC Dev Board, the USB TCPC code must be removed. */
+#ifdef TEST_ON_VERDIN_EVK
+#include "../../freescale/common/tcpc.h"
+
+#if IS_ENABLED(CONFIG_USB_TCPC)
+struct tcpc_port port;
+struct tcpc_port_config port_config = {
+	.i2c_bus = 6, /* i2c7 */
+	.addr = 0x52,
+	.port_type = TYPEC_PORT_DRP,
+	.disable_pd = true,
+};
+
+ulong tca_base;
+
+void tca_mux_select(enum typec_cc_polarity pol)
+{
+	u32 val;
+
+	if (!tca_base)
+		return;
+
+	/* reset XBar block */
+	setbits_le32(tca_base, BIT(9));
+
+	/* Set OP mode to System configure Mode */
+	clrbits_le32(tca_base + 0x10, 0x3);
+
+	val = readl(tca_base + 0x30);
+
+	WARN_ON((val & GENMASK(1, 0)) != 0x3);
+	WARN_ON((val & BIT(2)) != 0);
+	WARN_ON((val & BIT(3)) != 0);
+	WARN_ON((val & BIT(4)) != 0);
+
+	printf("tca pstate 0x%x\n", val);
+
+	setbits_le32(tca_base + 0x18, BIT(3));
+	udelay(1);
+
+	if (pol == TYPEC_POLARITY_CC1)
+		clrbits_le32(tca_base + 0x18, BIT(2));
+	else
+		setbits_le32(tca_base + 0x18, BIT(2));
+
+	udelay(1);
+
+	clrbits_le32(tca_base + 0x18, BIT(3));
+}
+
+static void setup_typec(void)
+{
+	int ret;
+
+	tca_base = USB1_BASE_ADDR + 0xfc000;
+
+	ret = tcpc_init(&port, port_config, &tca_mux_select);
+	if (ret) {
+		printf("%s: tcpc init failed, err=%d\n", __func__, ret);
+		return;
+	}
+}
+#endif
+#endif /* TEST_ON_VERDIN_EVK */
+
+#if IS_ENABLED(CONFIG_USB_DWC3)
+
+#define PHY_CTRL0			0xF0040
+#define PHY_CTRL0_REF_SSP_EN		BIT(2)
+#define PHY_CTRL0_FSEL_MASK		GENMASK(10, 5)
+#define PHY_CTRL0_FSEL_24M		0x2a
+#define PHY_CTRL0_FSEL_100M		0x27
+#define PHY_CTRL0_SSC_RANGE_MASK	GENMASK(23, 21)
+#define PHY_CTRL0_SSC_RANGE_4003PPM	(0x2 << 21)
+
+#define PHY_CTRL1			0xF0044
+#define PHY_CTRL1_RESET			BIT(0)
+#define PHY_CTRL1_COMMONONN		BIT(1)
+#define PHY_CTRL1_ATERESET		BIT(3)
+#define PHY_CTRL1_DCDENB		BIT(17)
+#define PHY_CTRL1_CHRGSEL		BIT(18)
+#define PHY_CTRL1_VDATSRCENB0		BIT(19)
+#define PHY_CTRL1_VDATDETENB0		BIT(20)
+
+#define PHY_CTRL2			0xF0048
+#define PHY_CTRL2_TXENABLEN0		BIT(8)
+#define PHY_CTRL2_OTG_DISABLE		BIT(9)
+
+#define PHY_CTRL6			0xF0058
+#define PHY_CTRL6_RXTERM_OVERRIDE_SEL	BIT(29)
+#define PHY_CTRL6_ALT_CLK_EN		BIT(1)
+#define PHY_CTRL6_ALT_CLK_SEL		BIT(0)
+
+static struct dwc3_device dwc3_device_data = {
+#ifdef CONFIG_SPL_BUILD
+	.maximum_speed = USB_SPEED_HIGH,
+#else
+	.maximum_speed = USB_SPEED_SUPER,
+#endif
+	.base = USB1_BASE_ADDR,
+	.dr_mode = USB_DR_MODE_PERIPHERAL,
+	.index = 0,
+	.power_down_scale = 2,
+};
+
+int dm_usb_gadget_handle_interrupts(struct udevice *dev)
+{
+	dwc3_uboot_handle_interrupt(dev);
+	return 0;
+}
+
+static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
+{
+	u32 value;
+
+	/* USB3.0 PHY signal fsel for 24M ref */
+	value = readl(dwc3->base + PHY_CTRL0);
+	value &= ~PHY_CTRL0_FSEL_MASK;
+	value |= FIELD_PREP(PHY_CTRL0_FSEL_MASK, PHY_CTRL0_FSEL_24M);
+	writel(value, dwc3->base + PHY_CTRL0);
+
+	/* Disable alt_clk_en and use internal MPLL clocks */
+	value = readl(dwc3->base + PHY_CTRL6);
+	value &= ~(PHY_CTRL6_ALT_CLK_SEL | PHY_CTRL6_ALT_CLK_EN);
+	writel(value, dwc3->base + PHY_CTRL6);
+
+	value = readl(dwc3->base + PHY_CTRL1);
+	value &= ~(PHY_CTRL1_VDATSRCENB0 | PHY_CTRL1_VDATDETENB0);
+	value |= PHY_CTRL1_RESET | PHY_CTRL1_ATERESET;
+	writel(value, dwc3->base + PHY_CTRL1);
+
+	value = readl(dwc3->base + PHY_CTRL0);
+	value |= PHY_CTRL0_REF_SSP_EN;
+	writel(value, dwc3->base + PHY_CTRL0);
+
+	value = readl(dwc3->base + PHY_CTRL2);
+	value |= PHY_CTRL2_TXENABLEN0 | PHY_CTRL2_OTG_DISABLE;
+	writel(value, dwc3->base + PHY_CTRL2);
+
+	udelay(10);
+
+	value = readl(dwc3->base + PHY_CTRL1);
+	value &= ~(PHY_CTRL1_RESET | PHY_CTRL1_ATERESET);
+	writel(value, dwc3->base + PHY_CTRL1);
+}
+#endif
+
+static int imx9_scmi_power_domain_enable(u32 domain, bool enable)
+{
+	return scmi_pwd_state_set(gd->arch.scmi_dev, 0, domain, enable ? 0 : BIT(30));
+}
+
+int board_early_init_f(void)
+{
+	/* UART1: A55 */
+	init_uart_clk(0);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	int ret;
+
+	ret = imx9_scmi_power_domain_enable(IMX95_PD_HSIO_TOP, true);
+	if (ret) {
+		printf("%s: Failed to enable PD HSIO for USB: %d\n", __func__, ret);
+		return ret;
+	}
+
+#ifdef TEST_ON_VERDIN_EVK
+	if (IS_ENABLED(CONFIG_USB_TCPC))
+		setup_typec();
+#endif
+
+	return 0;
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	int ret = 0;
+
+	if (index == 0) {
+		switch (init) {
+		case USB_INIT_DEVICE:
+			ret = imx9_scmi_power_domain_enable(IMX95_PD_HSIO_TOP, true);
+			if (ret)
+				goto error;
+
+			dwc3_nxp_usb_phy_init(&dwc3_device_data);
+
+#ifdef TEST_ON_VERDIN_EVK
+			if (IS_ENABLED(CONFIG_USB_TCPC)) {
+				ret = tcpc_setup_ufp_mode(&port);
+				if (ret)
+					goto error;
+			}
+#endif
+
+			ret = dwc3_uboot_init(&dwc3_device_data);
+			if (ret)
+				goto error;
+		break;
+		case USB_INIT_HOST:
+#ifdef TEST_ON_VERDIN_EVK
+			if (IS_ENABLED(CONFIG_USB_TCPC))
+				return tcpc_setup_dfp_mode(&port);
+#endif
+		break;
+		default:
+			return -1;
+		}
+	}
+
+	return 0;
+error:
+	printf("Board USB init failed: %d\n", ret);
+	return ret;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	int ret = 0;
+
+	if (index == 0) {
+		switch (init) {
+		case USB_INIT_DEVICE:
+				dwc3_uboot_exit(index);
+		break;
+		case USB_INIT_HOST:
+#ifdef TEST_ON_VERDIN_EVK
+			if (IS_ENABLED(CONFIG_USB_TCPC))
+				ret = tcpc_disable_src_vbus(&port);
+#endif
+		break;
+		default:
+			return -1;
+		}
+	}
+
+	return ret;
+}
+
+void board_quiesce_devices(void)
+{
+	int ret = 0;
+
+	ret = imx9_scmi_power_domain_enable(IMX95_PD_HSIO_TOP, false);
+	if (ret)
+		printf("%s: Failed to disable PD HSIO for USB: %d\n", __func__, ret);
+}
+
+int board_phys_sdram_size(phys_size_t *size)
+{
+	*size = PHYS_SDRAM_SIZE + PHYS_SDRAM_2_SIZE;
+
+	return 0;
+}
+
+#if IS_ENABLED(CONFIG_OF_LIBFDT) && IS_ENABLED(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	return ft_common_board_setup(blob, bd);
+}
+#endif

commit 97dc1edf687d5a1a9243f6f18fddee4413436134

board: toradex: smarc-imx95: clear bss segment in spl
When only the Cortex-A logical machine is reset the bss segment
is not zeroed out.
Fixes U-Boot reset or Linux rebppt not working due to the SPL
not able to load further boot firmware. Maybe there are
additional code paths which assume uninitalized variables
are zero.

Upstream-Status: Pending
Fixes: 8b13c1698fd6 ("board: toradex: Add initial support for toradex smarc imx95")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

board/toradex/smarc-imx95/spl.c
@@ -53,6 +53,9 @@ void board_init_f(ulong dummy)
 	u32 state = 0;
 	int ret;
 
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
 	if (IS_ENABLED(CONFIG_SPL_RECOVER_DATA_SECTION) && IS_ENABLED(CONFIG_SPL_BUILD))
 		spl_save_restore_data();
 