rborn-tx/u-boot

commit 3f76441dc642dc1ffb7a835ede2eff7d0774c87f

board: toradex: smarc-imx95: notify Embedded Controller when firmware is loaded
The Embedded Controller provides a 'Firmware Loaded' command to signal
when the initial firmware has been loaded.

The SoC USB1 (3.0), normally connected to a USB 3.0 hub in host mode
(providing USB1, USB2, USB3, and USB4 SMARC host interfaces), is the only
interface that supports USB recovery. A mux allows switching SoC USB1 to
the SMARC USB0 OTG interface (client mode) when FORCE_RECOVERY# is tied
to GND during boot, enabling USB recovery.
The EC drives the FORCE_RECOVERY# signal to ground to keep the USB
recovery configuration.
The SoC USB2 is a USB 2.0 interface and does not officially support
USB recovery.

Adds support for the 'Firmware Loaded' command in the Embedded
Controller, enabling the system to switch to the default USB
configuration, as soon as the SPL is loaded.
The remaining part of the boot process can use the default configuration.

Upstream-Status: Pending
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

board/toradex/smarc-imx95/spl.c
@@ -10,9 +10,11 @@
 #include <asm/sections.h>
 #include <asm/global_data.h>
 #include <clk.h>
+#include <dm/uclass.h>
 #include <dt-bindings/clock/fsl,imx95-clock.h>
 #include <dt-bindings/power/fsl,imx95-power.h>
 #include <hang.h>
+#include <i2c.h>
 #include <init.h>
 #include <log.h>
 #include <scmi_agent.h>
@@ -20,6 +22,8 @@
 #include <spl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define EC_I2C_BUS 3
 
 int spl_board_boot_device(enum boot_device boot_dev_spl)
 {
@@ -37,6 +41,33 @@ int spl_board_boot_device(enum boot_device boot_dev_spl)
 	default:
 		return BOOT_DEVICE_NONE;
 	}
+}
+
+static void ec_boot_notify(void)
+{
+	struct udevice *bus;
+	struct udevice *i2c_dev;
+	int ret;
+	u8 val = 0x03;
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C, EC_I2C_BUS, &bus);
+	if (ret) {
+		puts("Failed to get Toradex EC I2C BUS\n");
+		return;
+	}
+
+	ret = dm_i2c_probe(bus, 0x28, 0, &i2c_dev);
+	if (ret) {
+		puts("Toradex EC not found\n");
+		return;
+	}
+
+	/* TODO: Enable when USB support is complete (ELB-6256) */
+#if 0
+	ret = dm_i2c_write(i2c_dev, 0xD0, &val, 1);
+	if (ret)
+		puts("Cannot send command to Toradex EC\n");
+#endif
 }
 
 void spl_board_init(void)
@@ -87,6 +118,8 @@ void board_init_f(ulong dummy)
 		printf("scmi_pwd_state_get Failed %d for DDRMIX\n", ret);
 	else if (state == BIT(30))
 		panic("DDRMIX is powered OFF, Please initialize DDR with OEI\n");
+
+	ec_boot_notify();
 
 	board_init_r(NULL, 0);
 }

commit 07340114e50e3b4b3bf09cab8b6f1c36bef261f5

board: toradex: smarc-imx95: enable Embedded Controller firmware loaded command
Removed comment in order to enable Embedded Controller firmware loaded command.

Upstream-Status: Pending
Fixes: 3f76441dc642 ("board: toradex: smarc-imx95: notify Embedded Controller when firmware is loaded")
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

board/toradex/smarc-imx95/spl.c
@@ -62,12 +62,16 @@ static void ec_boot_notify(void)
 		return;
 	}
 
-	/* TODO: Enable when USB support is complete (ELB-6256) */
-#if 0
+	/* USB configuration before this command (when SoC starts in recovery):
+	 * - SoC USB1 -> Smarc USB0
+	 * - SoC USB2 -> NC
+	 * After the command:
+	 * - SoC USB1 -> SoM hub -> Smarc USB1, USB2, USB3 and USB4
+	 * - SoC USB2 -> Smarc USB0
+	 */
 	ret = dm_i2c_write(i2c_dev, 0xD0, &val, 1);
 	if (ret)
 		puts("Cannot send command to Toradex EC\n");
-#endif
 }
 
 void spl_board_init(void)