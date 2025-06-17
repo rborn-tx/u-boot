245024f1cd65f94a50c9333637158654cad1d1f6
João Paulo Gonçalves <joao.goncalves@toradex.com>
2025-06-17 08:52:35 -0300

    boot: Make ft_board_setup_ex() generic
    In some use cases, board-specific device tree changes must not be
    overwritten by system fixups. Although U-Boot provides
    ft_board_setup_ex() for this purpose, it is currently only used on TI
    Keystone. Make ft_board_setup_ex() to be a generic option, allowing its
    use by other architectures/boards. To maintain backward compatibility,
    enable it by default on TI Keystone.
    
    Upstream-Status: Backport [2b7c6b6f3ca34970be61c5189d263f94f0c94917]
    Signed-off-by: João Paulo Gonçalves <joao.goncalves@toradex.com>

--- boot/image-fdt.c
+++ boot/image-fdt.c
@@ -605,6 +605,7 @@ int image_setup_libfdt(struct bootm_headers *images, void *blob,
 {
 	ulong *initrd_start = &images->initrd_start;
 	ulong *initrd_end = &images->initrd_end;
+	bool skip_board_fixup = false;
 	int ret, fdt_ret, of_size;
 
 	if (IS_ENABLED(CONFIG_OF_ENV_SETUP)) {
@@ -655,18 +656,18 @@ int image_setup_libfdt(struct bootm_headers *images, void *blob,
 	fdt_fixup_pstore(blob);
 #endif
 	if (IS_ENABLED(CONFIG_OF_BOARD_SETUP)) {
-		const char *skip_board_fixup;
+		skip_board_fixup = (env_get_ulong("skip_board_fixup", 10, 0) == 1);
 
-		skip_board_fixup = env_get("skip_board_fixup");
-		if (skip_board_fixup && ((int)simple_strtol(skip_board_fixup, NULL, 10) == 1)) {
-			printf("skip board fdt fixup\n");
-		} else {
-			fdt_ret = ft_board_setup(blob, gd->bd);
-			if (fdt_ret) {
-				printf("ERROR: board-specific fdt fixup failed: %s\n",
-				       fdt_strerror(fdt_ret));
-				goto err;
-			}
+		if (skip_board_fixup)
+			printf("skip all board fdt fixup\n");
+	}
+
+	if (IS_ENABLED(CONFIG_OF_BOARD_SETUP) && !skip_board_fixup) {
+		fdt_ret = ft_board_setup(blob, gd->bd);
+		if (fdt_ret) {
+			printf("ERROR: board-specific fdt fixup failed: %s\n",
+			       fdt_strerror(fdt_ret));
+			goto err;
 		}
 	}
 	if (IS_ENABLED(CONFIG_OF_SYSTEM_SETUP)) {
@@ -713,10 +714,8 @@ int image_setup_libfdt(struct bootm_headers *images, void *blob,
 	if (lmb)
 		lmb_reserve(lmb, map_to_sysmem(blob), of_size);
 
-#if defined(CONFIG_ARCH_KEYSTONE)
-	if (IS_ENABLED(CONFIG_OF_BOARD_SETUP))
+	if (IS_ENABLED(CONFIG_OF_BOARD_SETUP_EXTENDED) && !skip_board_fixup)
 		ft_board_setup_ex(blob, gd->bd);
-#endif
 
 	return 0;
 err:
