rborn-tx/u-boot

commit e8a98365ae1c1c03718c6718b45983ca6d9c035c

board: toradex: verdin-imx95: fix fdtfile variable
Fix fdtfile variable, for verdin family the FDT name includes also the
variant.

This fixes the following tezi boot issue (and more)

| ## Executing script at 9c600000
| Bootargs: quiet video=HDMI-A-1:1280x720-16@60D rootfstype=squashfs root=/dev/ram autoinstall
| ## Loading kernel from FIT Image at 90400000 ...
| Could not find configuration node
| ERROR -2: can't get kernel image!

Related-to: ELB-6390
Upstream-Status: Pending
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

board/toradex/verdin-imx95/verdin-imx95.c
@@ -7,6 +7,7 @@
 #include <asm/io.h>
 #include <dt-bindings/power/fsl,imx95-power.h>
 #include <dwc3-uboot.h>
+#include <env.h>
 #include <fdt_support.h>
 #include <init.h>
 #include <linux/bitfield.h>
@@ -72,6 +73,39 @@ void board_quiesce_devices(void)
 	ret = imx9_scmi_power_domain_enable(IMX95_PD_NETC, false);
 	if (ret)
 		printf("%s: Failed to disable PD NETC for Ethernet: %d\n", __func__, ret);
+}
+
+static void select_dt_from_module_version(void)
+{
+	char variant[32];
+	char *env_variant = env_get("variant");
+	bool is_wifi = false;
+
+	if (IS_ENABLED(CONFIG_TDX_CFG_BLOCK)) {
+		/*
+		 * If we have a valid config block and it says we are a
+		 * module with Wi-Fi/Bluetooth make sure we use the -wifi
+		 * device tree.
+		 */
+		is_wifi = (tdx_hw_tag.prodid == VERDIN_IMX95H_8G_WIFI_BT_IT);
+	}
+
+	if (is_wifi)
+		strlcpy(&variant[0], "wifi", sizeof(variant));
+	else
+		strlcpy(&variant[0], "nonwifi", sizeof(variant));
+
+	if (strcmp(variant, env_variant)) {
+		printf("Setting variant to %s\n", variant);
+		env_set("variant", variant);
+	}
+}
+
+int board_late_init(void)
+{
+	select_dt_from_module_version();
+
+	return 0;
 }
 
 int board_phys_sdram_size(phys_size_t *size)

configs/verdin-imx95_defconfig
@@ -35,7 +35,7 @@ CONFIG_BOOTDELAY=1
 CONFIG_OF_SYSTEM_SETUP=y
 CONFIG_BOOTCOMMAND="bootflow scan -b"
 CONFIG_USE_PREBOOT=y
-CONFIG_PREBOOT="test -n ${fdtfile} || setenv fdtfile imx95-verdin-${fdt_board}.dtb"
+CONFIG_PREBOOT="test -n ${fdtfile} || setenv fdtfile imx95-verdin-${variant}-${fdt_board}.dtb"
 CONFIG_SYS_CBSIZE=2048
 CONFIG_SYS_PBSIZE=2074
 CONFIG_LOG=y
@@ -43,6 +43,7 @@ CONFIG_LOG=y
 CONFIG_DISPLAY_BOARDINFO_LATE=y
 CONFIG_ARCH_MISC_INIT=y
 CONFIG_BOARD_EARLY_INIT_F=y
+CONFIG_BOARD_LATE_INIT=y
 CONFIG_SPL_MAX_SIZE=0x30000
 CONFIG_SPL_HAS_BSS_LINKER_SECTION=y
 CONFIG_SPL_BSS_START_ADDR=0x204d6000

commit c113976ba18cafcd265fa8b36f9e987c9ff05d04

board: toradex: verdin-imx95: fix board variant selection
Check if env_variant is not NULL, before accessing it.

Fix the following crash

| "Synchronous Abort" handler, esr 0x96000006, far 0x0
| elr: 0000000090294520 lr : 0000000090205d60 (reloc)
| elr: 00000000fff8b520 lr : 00000000ffefcd60
| x0 : 00000000fdeec5e0 x1 : 0000000000000000
| x2 : 0000000000000004 x3 : 0000000000000000
| x4 : 00000000fdeec5e0 x5 : 00000000fdeec5e0
| x6 : 0000000000000004 x7 : 0000000000000004
| x8 : 00000000fdeec5e0 x9 : 0000000000000010
| x10: 0000000000000006 x11: 000000000001869f
| x12: 00000000fdf09870 x13: 0000000000000005
| x14: 0000000000000000 x15: 00000000fdeebccf
| x16: 00000000fff4ea04 x17: 0000000000000000
| x18: 00000000fdef6d30 x19: 00000000fff9a0cb
| x20: 0000000000000000 x21: 00000000fffb7008
| x22: 0000000000000000 x23: 0000000000000000
| x24: 0000000000000000 x25: 0000000000000000
| x26: 0000000000000000 x27: 0000000000000000
| x28: 0000000000000000 x29: 00000000fdeec5c0
|
| Code: 3900007f d65f03c0 aa0003e5 d2800003 (38636824)
| Resetting CPU ...

Related-to: ELB-6390
Upstream-Status: Pending
Fixes: e8a98365ae1c ("board: toradex: verdin-imx95: fix fdtfile variable")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

board/toradex/verdin-imx95/verdin-imx95.c
@@ -95,7 +95,7 @@ static void select_dt_from_module_version(void)
 	else
 		strlcpy(&variant[0], "nonwifi", sizeof(variant));
 
-	if (strcmp(variant, env_variant)) {
+	if (!env_variant || strcmp(variant, env_variant)) {
 		printf("Setting variant to %s\n", variant);
 		env_set("variant", variant);
 	}