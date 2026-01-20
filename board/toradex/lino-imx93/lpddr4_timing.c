rborn-tx/u-boot

commit 237866b449ef5803ad39fb076a3323673b91182f

board: toradex: add initial support for lino imx93
Add initial U-Boot support for Lino iMX93 SoM.

Upstream-Status: Pending
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

board/toradex/lino-imx93/Makefile
@@ -1,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2025 Toradex
+
+ifdef CONFIG_TARGET_LINO_IMX93
+obj-y += lino-imx93.o
+endif
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+obj-y += lpddr4x_timing.o
+endif

arch/arm/dts/imx93-imx91-lino-common.dtsi
@@ -1,0 +1,226 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+/*
+ * Copyright (C) 2025 Toradex
+ *
+ * Lino iMX93/iMX91 SoM
+ *
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx93
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx91
+ */
+
+#include <dt-bindings/gpio/gpio.h>
+#include <dt-bindings/interrupt-controller/irq.h>
+
+/ {
+	aliases {
+		i2c0 = &lpi2c2;
+		i2c1 = &lpi2c1;
+		ethernet0 = &eqos;
+		mmc0 = &usdhc1;
+		serial2 = &lpuart1;
+	};
+
+	chosen {
+		stdout-path = &lpuart1;
+	};
+
+	reserved-memory {
+		#address-cells = <2>;
+		#size-cells = <2>;
+		ranges;
+
+		// TODO: move to 0x87de0000 (below 256MB)
+		ele_reserved: memory@a4120000 {
+			compatible = "shared-dma-pool";
+			reg = <0 0xa4120000 0 0x100000>;
+			no-map;
+		};
+	};
+
+	reg_usb1_vbus: regulator-usb1-vbus {
+		compatible = "regulator-fixed";
+		/* Lino USB_1_EN (P27) */
+		gpios = <&som_gpio_expander 0 GPIO_ACTIVE_HIGH>;
+		enable-active-high;
+		regulator-max-microvolt = <5000000>;
+		regulator-min-microvolt = <5000000>;
+		regulator-name = "USB_1_EN";
+		startup-delay-us = <20000>;
+	};
+};
+
+&ele_fw2 {
+	memory-region = <&ele_reserved>;
+};
+
+/* Lino ETH_1 */
+&eqos {
+	pinctrl-names = "default";
+	pinctrl-0 = <&pinctrl_mdio>, <&pinctrl_eth1>;
+
+	mdio: mdio {
+		compatible = "snps,dwmac-mdio";
+		#address-cells = <1>;
+		#size-cells = <0>;
+		status = "disabled";
+	};
+};
+
+/* Lino I2C_1 */
+&lpi2c1 {
+	pinctrl-names = "default", "gpio";
+	pinctrl-0 = <&pinctrl_i2c1>;
+	pinctrl-1 = <&pinctrl_i2c1_gpio>;
+	#address-cells = <1>;
+	#size-cells = <0>;
+	clock-frequency = <100000>;
+	scl-gpios = <&gpio1 0 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN)>;
+	sda-gpios = <&gpio1 1 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN)>;
+	single-master;
+};
+
+/* On-module I2C - PMIC_I2C */
+&lpi2c2 {
+	pinctrl-names = "default", "gpio";
+	pinctrl-0 = <&pinctrl_pmic_i2c>;
+	pinctrl-1 = <&pinctrl_pmic_i2c_gpio>;
+	#address-cells = <1>;
+	#size-cells = <0>;
+	clock-frequency = <400000>;
+	scl-gpios = <&gpio1 2 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN)>;
+	sda-gpios = <&gpio1 3 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN)>;
+	single-master;
+	status = "okay";
+
+	pmic@32 {
+		compatible = "nxp,pf9453";
+		reg = <0x32>;
+		interrupt-parent = <&som_gpio_expander>;
+		interrupts = <5 IRQ_TYPE_EDGE_FALLING>;
+
+		regulators {
+			BUCK1 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V1.1_DDR (PMIC BUCK1)";
+				regulator-max-microvolt = <1100000>;
+				regulator-min-microvolt = <1100000>;
+			};
+
+			reg_vdd_soc: BUCK2 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+VDD_SOC (PMIC BUCK2)";
+				regulator-max-microvolt = <950000>;
+				regulator-min-microvolt = <612500>;
+				regulator-ramp-delay = <12500>;
+			};
+
+			reg_1v8: BUCK3 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V1.8 / +V1.8_ANA (PMIC BUCK3)";
+				regulator-max-microvolt = <1800000>;
+				regulator-min-microvolt = <1800000>;
+			};
+
+			reg_3v3: BUCK4 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V3.3 (PMIC BUCK4)";
+				regulator-max-microvolt = <3300000>;
+				regulator-min-microvolt = <3300000>;
+			};
+
+			reg_sd_3v3_1v8: LDO1 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V3.3_1.8_SD (PMIC LDO1)";
+				regulator-max-microvolt = <3300000>;
+				regulator-min-microvolt = <1800000>;
+			};
+
+			LDO2 {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V0.8_ANA (PMIC LDO2)";
+				regulator-max-microvolt = <800000>;
+				regulator-min-microvolt = <800000>;
+			};
+
+			LDO_SNVS {
+				regulator-always-on;
+				regulator-boot-on;
+				regulator-name = "+V1.8_BBSM (PMIC LDO_SNVS)";
+				regulator-max-microvolt = <1800000>;
+				regulator-min-microvolt = <1800000>;
+			};
+		};
+	};
+
+	som_gpio_expander: gpio@21 {
+		compatible = "nxp,pcal6408";
+		reg = <0x21>;
+		pinctrl-names = "default";
+		pinctrl-0 = <&pinctrl_gpio_exp_int>;
+		#interrupt-cells = <2>;
+		interrupt-controller;
+		interrupt-parent = <&gpio4>;
+		interrupts = <29 IRQ_TYPE_LEVEL_LOW>;
+		#gpio-cells = <2>;
+		gpio-controller;
+		gpio-line-names =
+			"Lino_P27", /* Lino USB_1_EN */
+			"Lino_P61", /* Lino USB_2_EN */
+			"TPM_SPI_CS#",
+			"Lino_P29", /* Lino USB_1_OC# */
+			"Lino_P63", /* Lino USB_2_OC# */
+			"PMIC_INT#",
+			"TPM_IRQ#",
+			"Lino_S66";
+	};
+};
+
+&lpm {
+	soc-supply = <&reg_vdd_soc>;
+	status = "okay";
+};
+
+/* Lino UART_3, used as the Linux Console */
+&lpuart1 {
+	pinctrl-names = "default";
+	pinctrl-0 = <&pinctrl_uart3>;
+};
+
+/* Lino USB_1 */
+&usbotg1 {
+	dr_mode = "otg";
+	adp-disable;
+	hnp-disable;
+	srp-disable;
+	usb-role-switch;
+	disable-over-current;
+	role-switch-default-mode = "peripheral";
+	samsung,picophy-dc-vol-level-adjust = <7>;
+	samsung,picophy-pre-emp-curr-control = <3>;
+};
+
+/* On-module eMMC */
+&usdhc1 {
+	pinctrl-names = "default", "state_100mhz", "state_200mhz";
+	pinctrl-0 = <&pinctrl_usdhc1>;
+	pinctrl-1 = <&pinctrl_usdhc1>;
+	pinctrl-2 = <&pinctrl_usdhc1>;
+	bus-width = <8>;
+	non-removable;
+	no-sdio;
+	no-sd;
+	status = "okay";
+};
+
+&wdog3 {
+	pinctrl-names = "default";
+	pinctrl-0 = <&pinctrl_wdog>;
+	fsl,ext-reset-output;
+	status = "okay";
+};

arch/arm/dts/imx93-imx91-lino-verdin-common.dtsi
@@ -1,0 +1,112 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+/*
+ * Copyright (C) 2025 Toradex
+ *
+ * Lino iMX93/iMX91 SoM on Lino-Verdin Adapter
+ *
+ * https://www.toradex.com/computer-on-modules/verdin-arm-family
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx93
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx91
+ */
+
+#include <dt-bindings/interrupt-controller/irq.h>
+#include <dt-bindings/net/ti-dp83867.h>
+
+/ {
+	connector {
+		compatible = "gpio-usb-b-connector", "usb-b-connector";
+		id-gpios = <&adapter_gpio_expander 1 GPIO_ACTIVE_HIGH>;
+		self-powered;
+		type = "micro";
+		vbus-supply = <&reg_usb1_vbus>;
+
+		port {
+			usb_1_connector: endpoint {
+				remote-endpoint = <&usb1_drd_sw>;
+			};
+		};
+	};
+};
+
+/* Lino ETH_1 */
+&eqos {
+	phy-handle = <&ethphy1>;
+	phy-mode = "rgmii-id";
+};
+
+/* Lino I2C_1 */
+&lpi2c1 {
+	status = "okay";
+
+	i2c-mux@70 {
+		compatible = "nxp,pca9543";
+		reg = <0x70>;
+		#address-cells = <1>;
+		#size-cells = <0>;
+
+		verdin_i2c_1: i2c@0 {
+			reg = <0>;
+			#address-cells = <1>;
+			#size-cells = <0>;
+		};
+
+		adapter_i2c: i2c@1 {
+			reg = <1>;
+			#address-cells = <1>;
+			#size-cells = <0>;
+
+			adapter_gpio_expander: gpio@21 {
+				compatible = "nxp,pcal6416";
+				reg = <0x21>;
+				pinctrl-names = "default";
+				pinctrl-0 = <&pinctrl_eth1_int>;
+				#interrupt-cells = <2>;
+				interrupt-controller;
+				interrupt-parent = <&gpio1>;
+				interrupts = <10 IRQ_TYPE_LEVEL_LOW>;
+				#gpio-cells = <2>;
+				gpio-controller;
+				gpio-line-names =
+					"SODIMM_244", /* Verdin PCIE_1_RESET#    */
+					"SODIMM_161", /* Verdin USB_1_ID         */
+					"SODIMM_255", /* Verdin CTRL_WAKE1_MICO# */
+					"ETH_1_INT#",
+					"CTRL_EN_WIFI",
+					"",
+					"",
+					"",
+					"SODIMM_42", /* Verdin I2S_2_BCLK  */
+					"SODIMM_48", /* Verdin I2S_2_D_IN  */
+					"SODIMM_46", /* Verdin I2S_2_D_OUT */
+					"SODIMM_44", /* Verdin I2S_2_SYNC  */
+					"SODIMM_64", /* Verdin QSPI_1_CS2# */
+					"SODIMM_66", /* Verdin QSPI_1_DQS  */
+					"SODIMM_68", /* Verdin QSPI_1_IO2  */
+					"SODIMM_62"; /* Verdin QSPI_1_IO3  */
+			};
+		};
+	};
+};
+
+/* Lino Ethernet MDIO */
+&mdio {
+	status = "okay";
+
+	ethphy1: ethernet-phy@1 {
+		reg = <1>;
+		interrupt-parent = <&adapter_gpio_expander>;
+		interrupts = <3 IRQ_TYPE_EDGE_FALLING>;
+		ti,rx-internal-delay = <DP83867_RGMIIDCTL_2_00_NS>;
+		ti,tx-internal-delay = <DP83867_RGMIIDCTL_2_00_NS>;
+	};
+
+};
+
+/* Lino USB_1 */
+&usbotg1 {
+	port {
+		usb1_drd_sw: endpoint {
+			remote-endpoint = <&usb_1_connector>;
+		};
+	};
+};

arch/arm/dts/imx93-imx91-lino-verdin-dev-common.dtsi
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+/*
+ * Copyright (C) 2025 Toradex
+ *
+ * Lino iMX93/iMX91 SoM on Lino-Verdin Adapter / Development carrier board
+ *
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx93
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx91
+ * https://www.toradex.com/products/carrier-board/verdin-development-board-kit
+ */
+
+/* Lino ETH_1 */
+&eqos {
+	status = "okay";
+};
+
+/* Lino UART_3, used as the Linux Console */
+&lpuart1 {
+	status = "okay";
+};
+
+/* Lino USB_1 */
+&usbotg1 {
+	status = "okay";
+};

arch/arm/dts/imx93-lino-verdin-dev.dts
@@ -1,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+/*
+ * Copyright (C) 2025 Toradex
+ *
+ * Lino iMX93 SoM on Lino-Verdin Adapter / Development carrier board
+ *
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx93
+ * https://www.toradex.com/products/carrier-board/verdin-development-board-kit
+ */
+
+/dts-v1/;
+
+#include "imx93.dtsi"
+#include "imx93-lino.dtsi"
+#include "imx93-imx91-lino-verdin-common.dtsi"
+#include "imx93-imx91-lino-verdin-dev-common.dtsi"
+
+/ {
+	model = "Toradex Lino iMX93 on Verdin Adapter plus Development Board";
+	compatible = "toradex,lino-imx93-verdin-dev",
+		     "toradex,lino-imx93-verdin",
+		     "toradex,lino-imx93",
+		     "fsl,imx93";
+};

arch/arm/dts/imx93-lino.dtsi
@@ -1,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+/*
+ * Copyright (C) 2025 Toradex
+ *
+ * Lino iMX93 SoM
+ *
+ * https://www.toradex.com/computer-on-modules/lino-arm-family/nxp-imx93
+ */
+
+#include "imx93-pinfunc.h"
+#include "imx93-imx91-lino-common.dtsi"
+
+&iomuxc {
+	/* Lino ETH MDIO */
+	pinctrl_mdio: eqosmdiogrp {
+		fsl,pins = <MX93_PAD_ENET1_MDC__ENET_QOS_MDC	0x1fe>, /* Lino P69 */
+			   <MX93_PAD_ENET1_MDIO__ENET_QOS_MDIO	0x1fe>; /* Lino P67 */
+	};
+
+	/* Lino ETH_1 RGMII */
+	pinctrl_eth1: eqosgrp {
+		fsl,pins = <MX93_PAD_ENET1_RD0__ENET_QOS_RGMII_RD0			0x1fe>, /* Lino P77 */
+			   <MX93_PAD_ENET1_RD1__ENET_QOS_RGMII_RD1			0x1fe>, /* Lino P79 */
+			   <MX93_PAD_ENET1_RD2__ENET_QOS_RGMII_RD2			0x1fe>, /* Lino P81 */
+			   <MX93_PAD_ENET1_RD3__ENET_QOS_RGMII_RD3			0x1fe>, /* Lino P83 */
+			   <MX93_PAD_ENET1_RXC__CCM_ENET_QOS_CLOCK_GENERATE_RX_CLK	0x1fe>, /* Lino P73 */
+			   <MX93_PAD_ENET1_RX_CTL__ENET_QOS_RGMII_RX_CTL		0x1fe>, /* Lino P75 */
+			   <MX93_PAD_ENET1_TD0__ENET_QOS_RGMII_TD0			0x1fe>, /* Lino P97 */
+			   <MX93_PAD_ENET1_TD1__ENET_QOS_RGMII_TD1			0x1fe>, /* Lino P95 */
+			   <MX93_PAD_ENET1_TD2__ENET_QOS_RGMII_TD2			0x1fe>, /* Lino P93 */
+			   <MX93_PAD_ENET1_TD3__ENET_QOS_RGMII_TD3			0x1fe>, /* Lino P91 */
+			   <MX93_PAD_ENET1_TXC__CCM_ENET_QOS_CLOCK_GENERATE_TX_CLK	0x1fe>, /* Lino P89 */
+			   <MX93_PAD_ENET1_TX_CTL__ENET_QOS_RGMII_TX_CTL		0x1fe>; /* Lino P85 */
+	};
+
+	/* Lino ETH_1 Interrupt */
+	pinctrl_eth1_int: gpio1io10grp {
+		fsl,pins = <MX93_PAD_PDM_BIT_STREAM1__GPIO1_IO10	0x139e>; /* Lino P65 */
+
+	};
+
+	/* On-module GPIO_EXP_INT# */
+	pinctrl_gpio_exp_int: gpio4io29grp {
+		fsl,pins = <MX93_PAD_CCM_CLKO4__GPIO4_IO29	0x139e>; /* GPIO_EXP_INT# */
+
+	};
+
+	/* Lino I2C_1 */
+	pinctrl_i2c1: lpi2c1grp {
+		fsl,pins = <MX93_PAD_I2C1_SCL__LPI2C1_SCL	0x40000b9e>, /* Lino P18 */
+			   <MX93_PAD_I2C1_SDA__LPI2C1_SDA	0x40000b9e>; /* Lino P16 */
+	};
+
+	/* Lino I2C_1 as GPIO */
+	pinctrl_i2c1_gpio: lpi2c1gpiogrp {
+		fsl,pins = <MX93_PAD_I2C1_SCL__GPIO1_IO00	0x40000b9e>, /* Lino P18 */
+			   <MX93_PAD_I2C1_SDA__GPIO1_IO01	0x40000b9e>; /* Lino P16 */
+	};
+
+	/* On-module PMIC I2C */
+	pinctrl_pmic_i2c: lpi2c2grp {
+		fsl,pins = <MX93_PAD_I2C2_SCL__LPI2C2_SCL	0x40000b9e>, /* PMIC_I2C_SCL */
+			   <MX93_PAD_I2C2_SDA__LPI2C2_SDA	0x40000b9e>; /* PMIC_I2C_SDA */
+	};
+
+	/* On-module PMIC I2C as GPIO */
+	pinctrl_pmic_i2c_gpio: lpi2c2gpiogrp {
+		fsl,pins = <MX93_PAD_I2C2_SCL__GPIO1_IO02	0x40000b9e>, /* PMIC_I2C_SCL */
+			   <MX93_PAD_I2C2_SDA__GPIO1_IO03	0x40000b9e>; /* PMIC_I2C_SDA */
+	};
+
+	/* Linux UART_3 */
+	pinctrl_uart3: uart1grp {
+		fsl,pins = <MX93_PAD_UART1_RXD__LPUART1_RX	0x39e>, /* Lino P17 */
+			   <MX93_PAD_UART1_TXD__LPUART1_TX	0x39e>; /* Lino P19 */
+	};
+
+	/* On-module eMMC */
+	pinctrl_usdhc1: usdhc1grp {
+		fsl,pins = <MX93_PAD_SD1_CLK__USDHC1_CLK	0x5fe>,      /* eMMC_CLK    */
+			   <MX93_PAD_SD1_CMD__USDHC1_CMD	0x400013fe>, /* eMMC_CMD    */
+			   <MX93_PAD_SD1_DATA0__USDHC1_DATA0	0x400013fe>, /* eMMC_DATA0  */
+			   <MX93_PAD_SD1_DATA1__USDHC1_DATA1	0x400013fe>, /* eMMC_DATA1  */
+			   <MX93_PAD_SD1_DATA2__USDHC1_DATA2	0x400013fe>, /* eMMC_DATA2  */
+			   <MX93_PAD_SD1_DATA3__USDHC1_DATA3	0x400013fe>, /* eMMC_DATA3  */
+			   <MX93_PAD_SD1_DATA4__USDHC1_DATA4	0x400013fe>, /* eMMC_DATA4  */
+			   <MX93_PAD_SD1_DATA5__USDHC1_DATA5	0x400013fe>, /* eMMC_DATA5  */
+			   <MX93_PAD_SD1_DATA6__USDHC1_DATA6	0x400013fe>, /* eMMC_DATA6  */
+			   <MX93_PAD_SD1_DATA7__USDHC1_DATA7	0x400013fe>, /* eMMC_DATA7  */
+			   <MX93_PAD_SD1_STROBE__USDHC1_STROBE	0x1580>;     /* eMMC_STROBE */
+	};
+
+	pinctrl_wdog: wdoggrp {
+		fsl,pins = <MX93_PAD_WDOG_ANY__WDOG1_WDOG_ANY 0x31e>; /* PMIC_WDOG_B */
+	};
+};

arch/arm/mach-imx/imx9/Kconfig
@@ -83,6 +83,11 @@ config TARGET_AQUILA_IMX95
 	bool "Support Toradex Aquila iMX95"
 	select IMX95
 
+config TARGET_LINO_IMX93
+	bool "Support Toradex Lino iMX93 module"
+	select IMX93
+	select IMX9_LPDDR4X
+
 config TARGET_TORADEX_SMARC_IMX95
 	bool "Support Toradex SMARC iMX95"
 	select IMX95
@@ -101,6 +106,7 @@ source "board/variscite/imx93_var_som/Kconfig"
 source "board/freescale/imx93_qsb/Kconfig"
 source "board/freescale/imx95_evk/Kconfig"
 source "board/toradex/aquila-imx95/Kconfig"
+source "board/toradex/lino-imx93/Kconfig"
 source "board/toradex/smarc-imx95/Kconfig"
 source "board/toradex/verdin-imx95/Kconfig"
 

board/toradex/lino-imx93/Kconfig
@@ -1,0 +1,36 @@
+if TARGET_LINO_IMX93
+
+config SYS_BOARD
+	default "lino-imx93"
+
+config SYS_VENDOR
+	default "toradex"
+
+config SYS_CONFIG_NAME
+	default "lino-imx93"
+
+config TDX_CFG_BLOCK
+	default y
+
+config TDX_CFG_BLOCK_2ND_ETHADDR
+	default y
+
+config TDX_CFG_BLOCK_DEV
+	default "0"
+
+# Toradex config block in eMMC, at the end of 1st "boot sector"
+config TDX_CFG_BLOCK_OFFSET
+	default "-512"
+
+config TDX_CFG_BLOCK_PART
+	default "1"
+
+config TDX_HAVE_EEPROM_EXTRA
+	default y
+
+config TDX_HAVE_MMC
+	default y
+
+source "board/toradex/common/Kconfig"
+
+endif

commit a00a10186dbaee0c6e088ec2f4b48002ad0e631c

board: toradex: lino-imx93: Fix memory timing for LPDDR4
Updates the LPDDR4 timing using the updated NXP tool version 25.12,
switch to use LPDDR4 instead of LPDDR4X and rename the timing file
accordingly.

Related-to: ELB-6852
Fixes: 237866b449ef ("board: toradex: add initial support for lino imx93")
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

board/toradex/lino-imx93/Makefile
@@ -7,5 +7,5 @@ endif
 
 ifdef CONFIG_SPL_BUILD
 obj-y += spl.o
-obj-y += lpddr4x_timing.o
+obj-y += lpddr4_timing.o
 endif

board/toradex/lino-imx93/lpddr4_timing.c
@@ -1,9 +1,9 @@
 /*
- * Copyright 2025 NXP
+ * Copyright 2026 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Code generated with DDR Tool v3.8.0_8.6-.
+ * Code generated with DDR Tool v3.9.0_8.7-.
  * DDR PHY FW2022.01 
  * Part number: lino-imx93
  */
@@ -39,11 +39,11 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
     {
         {
             {0x4e300100, 0x24AB321B},
-            {0x4e300104, 0xF8EE001B},
+            {0x4e300104, 0xDAEE001B},
             {0x4e300108, 0x2F2EE233},
             {0x4e30010C, 0x0005E18B},
             {0x4e300124, 0x1C030000},
-            {0x4e300160, 0x00009102},
+            {0x4e300160, 0x0000B102},
             {0x4e30016C, 0x35F00000},
             {0x4e300170, 0x8B0B0608},
             {0x4e300250, 0x00000028},
@@ -57,7 +57,7 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
         {
             {0x01, 0xE4},
             {0x02, 0x36},
-            {0x03, 0x32},
+            {0x03, 0x33},
             {0x0b, 0x46},
             {0x0c, 0x11},
             {0x0e, 0x11},
@@ -68,11 +68,11 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
     {
         {
             {0x4e300100, 0x12552100},
-            {0x4e300104, 0xF877000E},
+            {0x4e300104, 0xDA77000E},
             {0x4e300108, 0x1816B4AA},
             {0x4e30010C, 0x005101E6},
             {0x4e300124, 0x0E030000},
-            {0x4e300160, 0x00009101},
+            {0x4e300160, 0x0000B101},
             {0x4e30016C, 0x30900000},
             {0x4e300170, 0x8A0A0508},
             {0x4e300250, 0x00000014},
@@ -83,7 +83,7 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
         {
             {0x01, 0xB4},
             {0x02, 0x1B},
-            {0x03, 0x32},
+            {0x03, 0x33},
             {0x0b, 0x46},
             {0x0c, 0x11},
             {0x0e, 0x11},
@@ -94,11 +94,11 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
     {
         {
             {0x4e300100, 0x00061000},
-            {0x4e300104, 0xF855000A},
+            {0x4e300104, 0xDA55000A},
             {0x4e300108, 0x6E62FA48},
             {0x4e30010C, 0x0031010D},
             {0x4e300124, 0x04830000},
-            {0x4e300160, 0x00009100},
+            {0x4e300160, 0x0000B100},
             {0x4e30016C, 0x30000000},
             {0x4e300170, 0x89090408},
             {0x4e300250, 0x00000007},
@@ -109,7 +109,7 @@ static struct dram_fsp_cfg ddr_dram_fsp_cfg[] = {
         {
             {0x01, 0x94},
             {0x02, 0x9},
-            {0x03, 0x32},
+            {0x03, 0x33},
             {0x0b, 0x46},
             {0x0c, 0x11},
             {0x0e, 0x11},
@@ -138,18 +138,18 @@ static struct dram_cfg_param ddr_ddrphy_cfg[] = {
     {0x110a5, 0x4},
     {0x110a6, 0x6},
     {0x110a7, 0x5},
-    {0x1005f, 0x5ff},
-    {0x1015f, 0x5ff},
-    {0x1105f, 0x5ff},
-    {0x1115f, 0x5ff},
-    {0x11005f, 0x5ff},
-    {0x11015f, 0x5ff},
-    {0x11105f, 0x5ff},
-    {0x11115f, 0x5ff},
-    {0x21005f, 0x5ff},
-    {0x21015f, 0x5ff},
-    {0x21105f, 0x5ff},
-    {0x21115f, 0x5ff},
+    {0x1005f, 0x1ff},
+    {0x1015f, 0x1ff},
+    {0x1105f, 0x1ff},
+    {0x1115f, 0x1ff},
+    {0x11005f, 0x1ff},
+    {0x11015f, 0x1ff},
+    {0x11105f, 0x1ff},
+    {0x11115f, 0x1ff},
+    {0x21005f, 0x1ff},
+    {0x21015f, 0x1ff},
+    {0x21105f, 0x1ff},
+    {0x21115f, 0x1ff},
     {0x55, 0x1ff},
     {0x1055, 0x1ff},
     {0x2055, 0x1ff},
@@ -189,24 +189,24 @@ static struct dram_cfg_param ddr_ddrphy_cfg[] = {
     {0x21014d, 0x600},
     {0x21104d, 0x600},
     {0x21114d, 0x600},
-    {0x10049, 0xe00},
-    {0x10149, 0xe00},
-    {0x11049, 0xe00},
-    {0x11149, 0xe00},
-    {0x110049, 0xe00},
-    {0x110149, 0xe00},
-    {0x111049, 0xe00},
-    {0x111149, 0xe00},
-    {0x210049, 0xe00},
-    {0x210149, 0xe00},
-    {0x211049, 0xe00},
-    {0x211149, 0xe00},
-    {0x43, 0x60},
-    {0x1043, 0x60},
-    {0x2043, 0x60},
+    {0x10049, 0xe3f},
+    {0x10149, 0xe3f},
+    {0x11049, 0xe3f},
+    {0x11149, 0xe3f},
+    {0x110049, 0xe3f},
+    {0x110149, 0xe3f},
+    {0x111049, 0xe3f},
+    {0x111149, 0xe3f},
+    {0x210049, 0xe3f},
+    {0x210149, 0xe3f},
+    {0x211049, 0xe3f},
+    {0x211149, 0xe3f},
+    {0x43, 0x7f},
+    {0x1043, 0x7f},
+    {0x2043, 0x7f},
     {0x20018, 0x1},
     {0x20075, 0x4},
-    {0x20050, 0x0},
+    {0x20050, 0x11},
     {0x2009b, 0x2},
     {0x20008, 0x3a5},
     {0x120008, 0x1d3},
@@ -1248,23 +1248,23 @@ static struct dram_cfg_param ddr_fsp0_cfg[] = {
     {0x5400f, 0x100},
     {0x54012, 0x110},
     {0x54019, 0x36e4},
-    {0x5401a, 0x32},
+    {0x5401a, 0x33},
     {0x5401b, 0x1146},
     {0x5401c, 0x1108},
     {0x5401e, 0x4},
     {0x5401f, 0x36e4},
-    {0x54020, 0x32},
+    {0x54020, 0x33},
     {0x54021, 0x1146},
     {0x54022, 0x1108},
     {0x54024, 0x4},
     {0x54032, 0xe400},
-    {0x54033, 0x3236},
+    {0x54033, 0x3336},
     {0x54034, 0x4600},
     {0x54035, 0x811},
     {0x54036, 0x11},
     {0x54037, 0x400},
     {0x54038, 0xe400},
-    {0x54039, 0x3236},
+    {0x54039, 0x3336},
     {0x5403a, 0x4600},
     {0x5403b, 0x811},
     {0x5403c, 0x11},
@@ -1286,23 +1286,23 @@ static struct dram_cfg_param ddr_fsp1_cfg[] = {
     {0x5400f, 0x100},
     {0x54012, 0x110},
     {0x54019, 0x1bb4},
-    {0x5401a, 0x32},
+    {0x5401a, 0x33},
     {0x5401b, 0x1146},
     {0x5401c, 0x1108},
     {0x5401e, 0x4},
     {0x5401f, 0x1bb4},
-    {0x54020, 0x32},
+    {0x54020, 0x33},
     {0x54021, 0x1146},
     {0x54022, 0x1108},
     {0x54024, 0x4},
     {0x54032, 0xb400},
-    {0x54033, 0x321b},
+    {0x54033, 0x331b},
     {0x54034, 0x4600},
     {0x54035, 0x811},
     {0x54036, 0x11},
     {0x54037, 0x400},
     {0x54038, 0xb400},
-    {0x54039, 0x321b},
+    {0x54039, 0x331b},
     {0x5403a, 0x4600},
     {0x5403b, 0x811},
     {0x5403c, 0x11},
@@ -1324,23 +1324,23 @@ static struct dram_cfg_param ddr_fsp2_cfg[] = {
     {0x5400f, 0x100},
     {0x54012, 0x110},
     {0x54019, 0x994},
-    {0x5401a, 0x32},
+    {0x5401a, 0x33},
     {0x5401b, 0x1146},
     {0x5401c, 0x1100},
     {0x5401e, 0x4},
     {0x5401f, 0x994},
-    {0x54020, 0x32},
+    {0x54020, 0x33},
     {0x54021, 0x1146},
     {0x54022, 0x1100},
     {0x54024, 0x4},
     {0x54032, 0x9400},
-    {0x54033, 0x3209},
+    {0x54033, 0x3309},
     {0x54034, 0x4600},
     {0x54035, 0x11},
     {0x54036, 0x11},
     {0x54037, 0x400},
     {0x54038, 0x9400},
-    {0x54039, 0x3209},
+    {0x54039, 0x3309},
     {0x5403a, 0x4600},
     {0x5403b, 0x11},
     {0x5403c, 0x11},
@@ -1363,23 +1363,23 @@ static struct dram_cfg_param ddr_fsp0_2d_cfg[] = {
     {0x54010, 0x2080},
     {0x54012, 0x110},
     {0x54019, 0x36e4},
-    {0x5401a, 0x32},
+    {0x5401a, 0x33},
     {0x5401b, 0x1146},
     {0x5401c, 0x1108},
     {0x5401e, 0x4},
     {0x5401f, 0x36e4},
-    {0x54020, 0x32},
+    {0x54020, 0x33},
     {0x54021, 0x1146},
     {0x54022, 0x1108},
     {0x54024, 0x4},
     {0x54032, 0xe400},
-    {0x54033, 0x3236},
+    {0x54033, 0x3336},
     {0x54034, 0x4600},
     {0x54035, 0x811},
     {0x54036, 0x11},
     {0x54037, 0x400},
     {0x54038, 0xe400},
-    {0x54039, 0x3236},
+    {0x54039, 0x3336},
     {0x5403a, 0x4600},
     {0x5403b, 0x811},
     {0x5403c, 0x11},